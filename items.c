6cc6b807718901d0d8d9bbd1a243be44d3fe166d
Jay Grizzard <jgrizzard@box.com>
2014-08-20 19:23:42 -0700

    Stats and tests for 'refhang' problem
    Add "reflocked" counter to "stats" and "stats items". Not sure if this is a
    good name for that or not.
    
    Add tests to verify a fully 'locked' LRU correctly reports oom

--- items.c
+++ items.c
@@ -29,6 +29,7 @@ typedef struct {
     uint64_t expired_unfetched;
     uint64_t evicted_unfetched;
     uint64_t crawler_reclaimed;
+    uint64_t reflocked;
 } itemstats_t;
 
 static item *heads[LARGEST_ID];
@@ -137,6 +138,7 @@ retry_search:
              */
             do_item_update_nolock(search);
             refcount_decr(&search->refcount);
+            itemstats[id].reflocked++;
             /* Old rare bug could cause a refcount leak. We haven't seen
              * it in years, but we leave this code in to prevent failures
              * just in case */
@@ -477,6 +479,7 @@ void do_item_stats_totals(ADD_STAT add_stats, void *c) {
         totals.evicted += itemstats[i].evicted;
         totals.reclaimed += itemstats[i].reclaimed;
         totals.crawler_reclaimed += itemstats[i].crawler_reclaimed;
+        totals.reflocked += itemstats[i].reflocked;
     }
     APPEND_STAT("expired_unfetched", "%llu",
                 (unsigned long long)totals.expired_unfetched);
@@ -488,6 +491,8 @@ void do_item_stats_totals(ADD_STAT add_stats, void *c) {
                 (unsigned long long)totals.reclaimed);
     APPEND_STAT("crawler_reclaimed", "%llu",
                 (unsigned long long)totals.crawler_reclaimed);
+    APPEND_STAT("reflocked", "%llu",
+                (unsigned long long)totals.reflocked);
 }
 
 void do_item_stats(ADD_STAT add_stats, void *c) {
@@ -522,6 +527,8 @@ void do_item_stats(ADD_STAT add_stats, void *c) {
                                 "%llu", (unsigned long long)itemstats[i].evicted_unfetched);
             APPEND_NUM_FMT_STAT(fmt, i, "crawler_reclaimed",
                                 "%llu", (unsigned long long)itemstats[i].crawler_reclaimed);
+            APPEND_NUM_FMT_STAT(fmt, i, "reflocked",
+                                "%llu", (unsigned long long)itemstats[i].reflocked);
         }
     }
 
